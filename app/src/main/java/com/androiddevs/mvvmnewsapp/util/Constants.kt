afebfe381372a62ec7b0ea0b10d890c7b3df032f
Gabriel Mc Gann <32915926+DevMcGann@users.noreply.github.com>
2020-09-20 23:12:05 -0300

    finished

---
+++ app/src/main/java/com/androiddevs/mvvmnewsapp/util/Constants.kt
@@ -0,0 +1,10 @@
+package com.androiddevs.mvvmnewsapp.util
+
+class Constants {
+    companion object {
+        const val API_KEY = "e98b70a5399e4e919ef805eab8b3d721"
+        const val BASE_URL = "https://newsapi.org"
+        const val SEARCH_NEWS_TIME_DELAY = 500L
+        const val QUERY_PAGE_SIZE = 20
+    }
+}
